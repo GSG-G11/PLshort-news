71fa309495cf13be856ee03b2dda3ad9526bf563
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2022-03-01 12:43:57 +0200

    Add src Folder R #2

---
+++ src/tests/router.test.js
@@ -0,0 +1,19 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-undef */
+// const request = require('supertest');
+
+// const app = require('../app');
+
+// describe('Test the root path', () => {
+//   test('It should response the GET method', (done) => {
+//     // request(app)
+//     //   .get('/')
+//     //   .expect(200)
+//     //   .expect('Content-Type', /json/)
+//     //   .end((error, response) => {
+//     //     if (error) return done(error);
+//     //     expect(response.statusCode).toBe(200);
+//     //     done();
+//     //   });
+//   });
+// });
