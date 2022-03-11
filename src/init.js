2b1044074c2812958085d4a024828208a1e6b150
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2022-03-11 12:47:10 +0200

    fix bug

---
+++ src/init.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { join } = require('path');
+const compression = require('compression');
+const { hidePoweredBy } = require('helmet');
+const cookieParser = require('cookie-parser');
+const router = require('./routes/index');
+const middlewareAuth = require('./controllers/middleware');
+
+module.exports = (app) => {
+  app.use(cookieParser());
+  app.use(compression());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(hidePoweredBy());
+
+  // set cache age (maxAge) to 30 days
+  app.use(
+    express.static(join(__dirname, '..', 'public'), {
+      maxAge: '30d',
+    }),
+  );
+
+  app.use(middlewareAuth);
+  app.use(router);
+};
