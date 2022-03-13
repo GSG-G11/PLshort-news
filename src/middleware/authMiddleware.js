8779b11ebe48d09aa395fbb3799a5e92cfb75cd9
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2022-03-13 21:22:45 +0200

    Use jwt to auth and use jsonwebtoken to create token

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -19,4 +19,21 @@ module.exports = {
       return next();
     });
   },
+  redirectToDefault: ({ cookies }, res) => {
+    const { accessToken } = cookies;
+    const token = accessToken && accessToken.split(' ')[1];
+    if (!token)
+      return res
+        .status(401)
+        .sendFile(join(__dirname, '..', '..', 'public', 'error', '401.html'));
+
+    return verify(token, process.env.ACCESS_TOKEN_SECRET, (err) => {
+      if (err) {
+        return res
+          .status(401)
+          .sendFile(join(__dirname, '..', '..', 'public', 'error', '401.html'));
+      }
+      return res.status(302).redirect('/home');
+    });
+  },
 };
