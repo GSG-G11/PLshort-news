bd499cc6550e197420f34e9bc65e5ea962663f94
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2022-03-13 21:43:48 +0200

    Use jwt to auth and use jsonwebtoken to create token

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -5,27 +5,19 @@ module.exports = {
   authenticateToken: ({ cookies }, res, next) => {
     const { accessToken } = cookies;
     const token = accessToken && accessToken.split(' ')[1];
-    if (!token)
-      return res
-        .status(401)
-        .sendFile(join(__dirname, '..', '..', 'public', 'error', '401.html'));
+    if (!token) return res.status(302).redirect('/');
 
     return verify(token, process.env.ACCESS_TOKEN_SECRET, (err) => {
       if (err) {
-        return res
-          .status(401)
-          .sendFile(join(__dirname, '..', '..', 'public', 'error', '401.html'));
+        return res.status(302).redirect('/');
       }
       return next();
     });
   },
-  redirectToDefault: ({ cookies }, res) => {
+  redirectToDefault: ({ cookies }, res, next) => {
     const { accessToken } = cookies;
     const token = accessToken && accessToken.split(' ')[1];
-    if (!token)
-      return res
-        .status(401)
-        .sendFile(join(__dirname, '..', '..', 'public', 'error', '401.html'));
+    if (!token) return next();
 
     return verify(token, process.env.ACCESS_TOKEN_SECRET, (err) => {
       if (err) {
